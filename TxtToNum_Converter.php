3892709b0c097789d598c52792a447318be98393
Serge-Goujon <goujon@mail.ru>
2017-02-18 02:08:50 +0300

    Minor code formatting 3 (PSR2 + PHPMD)

--- TxtToNum_Converter.php
+++ TxtToNum_Converter.php
@@ -1,22 +1,62 @@
 <?php
 
+$num = 10000011;
+$result = ConvertToText($num);
+echo $result . "<br />";
+echo $num;
+
 
 function convertToText(float $numberToConvert): string
 {
-    $Units = [
-        "ноль ","один ","два ","три ","четыре ","пять ","шесть ","семь ","восемь ","девять ","десять ","одиннадцать ",
-        "двенадцать ","тринадцать ","четырнадцать ","пятнадцать ","шестнадцать ","семнадцать ","восемнадцать ",
-        "девятнадцать ","двадцать "
+    $arrUnits = [
+        "ноль ",
+        "один ",
+        "два ",
+        "три ",
+        "четыре ",
+        "пять ",
+        "шесть ",
+        "семь ",
+        "восемь ",
+        "девять ",
+        "десять ",
+        "одиннадцать ",
+        "двенадцать ",
+        "тринадцать ",
+        "четырнадцать ",
+        "пятнадцать ",
+        "шестнадцать ",
+        "семнадцать ",
+        "восемнадцать ",
+        "девятнадцать ",
+        "двадцать "
     ];
-    $Tens = [
-        "empty","десять ","двадцать ","тридцать ","сорок ","пятьдесят ","шестьдесят ","семьдесят ","восемьдесят ",
+    $arrTens = [
+        "empty",
+        "десять ",
+        "двадцать ",
+        "тридцать ",
+        "сорок ",
+        "пятьдесят ",
+        "шестьдесят ",
+        "семьдесят ",
+        "восемьдесят ",
         "девяносто "
     ];
-    $Hundreds = [
-        "empty","сто ","двести ","триста ","четыреста ","пятьсот ","шестьсот ","семьсот ","восемьсот ","девятьсот "
+    $arrHundreds = [
+        "empty",
+        "сто ",
+        "двести ",
+        "триста ",
+        "четыреста ",
+        "пятьсот ",
+        "шестьсот ",
+        "семьсот ",
+        "восемьсот ",
+        "девятьсот "
     ];
 
-    $Registry = array(
+    $arrMagnitude = array(
         array(0, "копейка ", "копейки ", "копеек "), //future functionality
         array(0, "рубль ", "рубля ", "рублей "),
         array(0, "тысяча ", "тысячи ", "тысяч "),
@@ -24,61 +64,63 @@ function convertToText(float $numberToConvert): string
         array(0, "миллиард ", "миллиарда ", "миллиардов "),
         array(0, "триллион ", "триллиона ", "триллионов ")
     );
+
+    $errMessage = null;
     if ($numberToConvert < 0 || $numberToConvert > 99999999999999) {
-        return $error = "Error: Input number should be between 1 and 99'999'999'999'999";
+        return $errMessage = "Error: Input number should be between 1 and 99'999'999'999'999";
     }
 
-    $Chunks = array(); // TODO: Convert all chunks to string type from int.
-    $Chunks[] = 0;
+    $arrChunks = array(); // TODO: Convert all chunks to string type from int.
+    $arrChunks[] = 0;
     $reversedValue = strrev(strval($numberToConvert));
     $reversedSize = strlen($reversedValue);
 
     for ($i = 0; $i < $reversedSize; $i += 3) {
-        $Chunks[] = strrev(substr($reversedValue, $i, 3));
+        $arrChunks[] = strrev(substr($reversedValue, $i, 3));
     }
 
-    $numGroups = count($Chunks) - 1;
+    $numGroups = count($arrChunks) - 1;
     $fullResult = null;
 
     for ($i = $numGroups; $i >= 1; $i--) { // Main big cycle
 
         if ($i == 2) {
-            $Units[1] = "одна ";
-            $Units[2] = "две ";
+            $arrUnits[1] = "одна ";
+            $arrUnits[2] = "две ";
         } else {
-            $Units[1] = "один ";
-            $Units[2] = "два ";
+            $arrUnits[1] = "один ";
+            $arrUnits[2] = "два ";
         }
 
         $resArray = array();
         $preResult = null;
-        $numLen = strlen($Chunks[$i]);
+        $numLen = strlen($arrChunks[$i]);
 
         $centis = null;
         if ($numLen == 3) {
-            $centis = intval(substr($Chunks[$i], 0, 1));
+            $centis = intval(substr($arrChunks[$i], 0, 1));
         }
 
         if ($centis != 0) {
-            $preResult .= $Hundreds[$centis];
+            $preResult .= $arrHundreds[$centis];
         }
 
-        $decimals = intval(substr($Chunks[$i], -2));
+        $decimals = intval(substr($arrChunks[$i], -2));
 
         if ($decimals >= 0 && $decimals < 21) {
-            $preResult .= $Units[$decimals];
+            $preResult .= $arrUnits[$decimals];
         } elseif ($decimals != 0) {
-            $preResult .= $Tens[intval(substr($Chunks[$i], -2, 1))];
+            $preResult .= $arrTens[intval(substr($arrChunks[$i], -2, 1))];
 
             if (intval(substr($decimals, -1)) != 0) {
-                $preResult .= $Units[intval(substr($Chunks[$i], -1))];
+                $preResult .= $arrUnits[intval(substr($arrChunks[$i], -1))];
             }
         }
 
-        if ($Chunks[$i] != '000') {
-            $preResult .= getGroupname($Registry, $i, $Chunks[$i]);
-        } elseif ($Chunks[$i] == '000' && $i == 1) {
-            $preResult .= getGroupname($Registry, $i, $Chunks[$i]);
+        if ($arrChunks[$i] != '000') {
+            $preResult .= getGroupname($arrMagnitude, $i, $arrChunks[$i]);
+        } elseif ($arrChunks[$i] == '000' && $i == 1) {
+            $preResult .= getGroupname($arrMagnitude, $i, $arrChunks[$i]);
         }
 
         $resArray[$i] = $preResult;
@@ -92,25 +134,25 @@ function getGroupname(array $group, int $gnum, string $number): string
 {
 
     $subResult = null;
-    $ns = strlen($number);
-    $nx = substr($number, -2);
+    $nls = strlen($number);
+    $nxs = substr($number, -2);
 
-    if ($ns > 1 && $nx == 11 || $nx == 12 || $nx == 13 || $nx == 14) {
-        $subResult = $group[$gnum][3];
-    } else {
-        switch (substr($number, -1)) {
-            case 1:
-                $subResult = $group[$gnum][1];
-                break;
-            case 2:
-            case 3:
-            case 4:
-                $subResult = $group[$gnum][2];
-                break;
-            default:
-                $subResult = $group[$gnum][3];
-                break;
-        }
+    if ($nls > 1 && $nxs >= 11 && $nxs <= 14) {
+        return $subResult = $group[$gnum][3];
+    }
+
+    switch (substr($number, -1)) {
+        case 1:
+            $subResult = $group[$gnum][1];
+            break;
+        case 2:
+        case 3:
+        case 4:
+            $subResult = $group[$gnum][2];
+            break;
+        default:
+            $subResult = $group[$gnum][3];
+            break;
     }
 
     return $subResult;
