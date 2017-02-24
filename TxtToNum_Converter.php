f8f092e4cd3cba618d453fdf4d2b41dce08178a4
Serge-Goujon <goujon@mail.ru>
2017-02-24 18:21:11 +0300

    Moved text data arrays to JSON file.

--- TxtToNum_Converter.php
+++ TxtToNum_Converter.php
@@ -1,54 +1,53 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-function convertToText(float $numberToConvert): string
+function convertToText(float $inputNumber): string
 {
-    if ($numberToConvert < 0 || $numberToConvert > 99999999999999) {
-        return $message = "Error: Input number should be between 0 and 99'999'999'999'999";
+    if ($inputNumber < 0 || $inputNumber > 99999999999999) {
+        return $message = 'Error: Input number should be between 0 and 99\'999\'999\'999\'999';
     }
 
-    if ($numberToConvert == 0) {  //avoiding all calculations to display such a simple result
-        return $message = "ноль рублей";
+    if ($inputNumber == 0) {  //avoiding all calculations to display such a simple result
+        return $message = 'ноль рублей';
     }
 
-    $result = num2txt($numberToConvert);
+    $result = num2txt($inputNumber);
 
     return $result;
 }
 
-function num2txt(float $numberToConvert): string
+function num2txt(float $numberConvert): string
 {
     $allArrays = getArrays();
-    $arrUnits = $allArrays[0];
-    $arrTens = $allArrays[1];
-    $arrHundreds = $allArrays[2];
-    $arrMagnitude = $allArrays[3];
+    list($arrUnits, $arrTens, $arrHundreds, $arrMagnitude) = $allArrays;
 
-    $arrChunks = getChunks($numberToConvert);
-    $numGroups = count($arrChunks) - 1;
+    $arrChunks = getChunks($numberConvert);
+    $numGroups = count($arrChunks);
     $fullResult = null;
 
     for ($i = $numGroups; $i >= 1; $i--) {
         $preResult = null;
-        $currChunk = $arrChunks[$i];
+        $currChunk = $arrChunks[$i - 1];
         $numLen = strlen($currChunk);
-
         $arrUnits = fixArray($i, $arrUnits);
 
-        $centis = intval(substr($currChunk, 0, 1));
+        $centis = intval($currChunk / 100);
+        $decimals = $currChunk - $centis * 100;
+
         if ($numLen == 3 && $centis != 0) {
-            $preResult .= $arrHundreds[$centis];
+            $preResult .= $arrHundreds[$centis - 1];
         }
-
-        $decimals = intval(substr($currChunk, -2));
-        if ($decimals > 0 && $decimals <= 19) {
-            $preResult .= $arrUnits[$decimals];
-        } else {
-            $preResult .= $arrTens[substr($currChunk, -2, 1)];
-            $preResult .= $arrUnits[substr($currChunk, -1)];
+        if ($decimals > 0 && $decimals < 20) {
+            $preResult .= $arrUnits[$decimals - 1];
+            $decimals = 0;
         }
-
+        if ($decimals != 0) {
+            $preResult .= $arrTens[intval($decimals / 10) - 1];
+        }
+        if ($decimals % 10 != 0) {
+            $preResult .= $arrUnits[$decimals % 10 - 1];
+        }
         if ($currChunk != 0 || $i == 1) {
             $preResult .= getMagnitude($arrMagnitude, $i, $currChunk);
         }
@@ -61,26 +60,29 @@ function num2txt(float $numberToConvert): string
 
 function getMagnitude(array $group, int $gnum, string $number): string
 {
-
     $subResult = null;
     $nls = strlen($number);
     $nxs = substr($number, -2);
 
+    //    if ($gnum == 1) { //If only number needed, without currency word(s)
+    //        return "";
+    //    }
+
     if ($nls > 1 && $nxs >= 11 && $nxs <= 14) {
-        return $subResult = $group[$gnum][3];
+        return $subResult = $group[$gnum][2];
     }
 
     switch (substr($number, -1)) {
         case 1:
-            $subResult = $group[$gnum][1];
+            $subResult = $group[$gnum][0];
             break;
         case 2:
         case 3:
         case 4:
-            $subResult = $group[$gnum][2];
+            $subResult = $group[$gnum][1];
             break;
         default:
-            $subResult = $group[$gnum][3];
+            $subResult = $group[$gnum][2];
             break;
     }
 
@@ -89,9 +91,7 @@ function getMagnitude(array $group, int $gnum, string $number): string
 
 function getChunks(float $inputNumber): array
 {
-
     $arrCh = array();
-    $arrCh[] = 0;
     $reversedValue = strrev(strval($inputNumber));
     $reversedSize = strlen($reversedValue);
 
@@ -104,13 +104,12 @@ function getChunks(float $inputNumber): array
 
 function fixArray(int $fem, array $arr): array
 {
-
     if ($fem == 2) {
-        $arr[1] = "одна ";
-        $arr[2] = "две ";
+        $arr[1] = 'одна ';
+        $arr[2] = 'две ';
     } else {
-        $arr[1] = "один ";
-        $arr[2] = "два ";
+        $arr[1] = 'один ';
+        $arr[2] = 'два ';
     }
 
     return $arr;
@@ -118,25 +117,8 @@ function fixArray(int $fem, array $arr): array
 
 function getArrays(): array
 {
-    //@formatter:off
-    $arrMagnitude = [
-                     array(0,"копейка ", "копейки ", "копеек "), //future functionality
-                     array(0,"рубль ", "рубля ", "рублей "),
-                     array(0,"тысяча ", "тысячи ", "тысяч "),
-                     array(0,"миллион ", "миллиона ", "миллионов "),
-                     array(0,"миллиард ", "миллиарда ", "миллиардов "),
-                     array(0,"триллион ", "триллиона ", "триллионов "),
-                     array(0,"квадриллион ", "квадриллиона ", "квадриллионов "),
-                     array(0,"квинтиллион ", "квинтиллиона ", "квинтиллионов ")
-                    ];
-    $arrUnits =     ["","один ","два ","три ","четыре ","пять ","шесть ","семь ", "восемь ", "девять ",
-                    "десять ","одиннадцать ","двенадцать ","тринадцать ","четырнадцать ","пятнадцать ",
-                    "шестнадцать ","семнадцать ","восемнадцать ","девятнадцать "];
-    $arrTens =      ["","десять ","двадцать ","тридцать ","сорок ","пятьдесят ","шестьдесят ","семьдесят ",
-                    "восемьдесят ","девяносто "];
-    $arrHundreds =  ["","сто ","двести ","триста ","четыреста ","пятьсот ","шестьсот ","семьсот ",
-                    "восемьсот ","девятьсот "];
-    //@formatter:on
+    $data = file_get_contents('data.json');
+    $allArrays = json_decode($data, true, 4);
 
-    return array($arrUnits, $arrTens, $arrHundreds, $arrMagnitude);
+    return $allArrays;
 }
--- test.php
+++ test.php
@@ -3,5 +3,21 @@ declare(strict_types=1);
 
 require_once 'TxtToNum_Converter.php';
 
-$num = 10000000001;
-echo ConvertToText($num);
+$num = 90101101000621;
+$strnum = ConvertToText($num);
+
+echo mbUcfirst($strnum, "UTF-8", true);
+
+function mbUcfirst($str, $encoding = "UTF-8", $lowerStrend = false)
+{
+    $firstLetter = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding);
+    $strEnd = null;
+    if ($lowerStrend) {
+        $strEnd = mb_strtolower(mb_substr($str, 1, mb_strlen($str, $encoding), $encoding), $encoding);
+    } else {
+        $strEnd = mb_substr($str, 1, mb_strlen($str, $encoding), $encoding);
+    }
+    $str = $firstLetter . $strEnd;
+
+    return $str;
+}
