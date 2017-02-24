9e90dae5d503b7f520d9f7c222cbd9965161b0b7
Serge-Goujon <goujon@mail.ru>
2017-02-25 01:43:57 +0300

    Changes to .gi and test.php.

--- test.php
+++ test.php
@@ -1,12 +1,22 @@
 <?php
 declare(strict_types=1);
 
+if (!file_exists('data.json')) {
+    exit('ERROR: No data file found. Please upload data.json in the same dir with this one.');
+}
+
 require_once 'TxtToNum_Converter.php';
+$num = 0;
 
-$num = 90101099000621;
-$strnum = ConvertToText($num);
+if (isset($_GET['number'])) {
+    $data = floatval($_GET['number']);
 
-echo mbUcfirst($strnum, "UTF-8", true);
+    if ($data >= 0) {
+        $num = $data;
+        $strnum = ConvertToText($num);
+        echo $num, ' : ', mbUcfirst($strnum, "UTF-8", true), '<br>';
+    }
+}
 
 function mbUcfirst($str, $encoding = "UTF-8", $lowerStrend = false)
 {
@@ -21,3 +31,18 @@ function mbUcfirst($str, $encoding = "UTF-8", $lowerStrend = false)
 
     return $str;
 }
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Testing Converter</title>
+</head>
+<body>
+<form action="test.php" method="get">
+    <input type="text" name="number" value="<?php echo $num; ?>" title="Enter number: ">
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
